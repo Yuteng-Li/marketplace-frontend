21329bdf9807cfc113c294dcadcef78c108af6b3
Patrick chan <patchan36@yahoo.com>
2022-09-20 16:18:26 -0700

    Save work on display
    need to pull new main and combine

--- marketplace-frontend/src/app/app.module.ts
+++ marketplace-frontend/src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemGirdComponent } from './item-gird/item-gird.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemGirdComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule
+
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ marketplace-frontend/src/app/item-gird/item-gird.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-gird',
+  templateUrl: './item-gird.component.html',
+  styleUrls: ['./item-gird.component.css']
+})
+export class ItemGirdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
