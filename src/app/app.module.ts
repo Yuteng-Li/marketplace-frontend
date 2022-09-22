2f9f9088f9d370e9de7036bec57cd2bba31c7507
Andrew <23265028+AndrewMurillo@users.noreply.github.com>
2022-09-22 13:13:47 -0700

    Add routing to category page and out to products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,11 @@ import { CategoriesComponent } from './categories/categories.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'categories', component: CategoriesComponent},
+      {path: 'item-gird/:category', component: ItemGirdComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
