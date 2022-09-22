323bb4633ee9d02da29eb45e871f3293a4cee9e7
garciale24 <c.garciale24@gmail.com>
2022-09-21 17:52:10 -0700

    better css

--- src/app/previous-orders/previous-orders.component.ts
+++ src/app/previous-orders/previous-orders.component.ts
@@ -9,6 +9,10 @@ export class PreviousOrdersComponent implements OnInit {
   today = new Date();
   yesterday = new Date((new Date()).valueOf() - 1000*60*60*24);
   dayBeforeYesterday = new Date((new Date()).valueOf() - 1000*60*60*24*2);
+
+  today2 = new Date((new Date()).valueOf() - 1000*60*60*24);
+  yesterday2 = new Date((new Date()).valueOf() - 1000*60*60*24*2);
+  dayBeforeYesterday2 = new Date((new Date()).valueOf() - 1000*60*60*24*3);
   previousOrders = [
     {
       "OrderID": 1,
@@ -49,14 +53,17 @@ export class PreviousOrdersComponent implements OnInit {
       "AddressID": 4,
       "Price": 8.99,
       "CreditCardID": 4,
-      "DateOrdered": this.dayBeforeYesterday,
-      "DateShipped": this.yesterday,
-      "DateDelivered": this.today,
+      "DateOrdered": this.dayBeforeYesterday2,
+      "DateShipped": null,
+      "DateDelivered": null,
       "OrderStatus": "Not Shipped Yet"
     }
   ]
   emptyList = []
 
+  sortedOrders = this.previousOrders.sort((a: any, b: any) => {
+    return a.DateOrdered.valueOf() - b.DateOrdered.valueOf();
+  });
 
 
 
