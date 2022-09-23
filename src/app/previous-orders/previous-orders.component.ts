b993ce1f3a64c9cac77c05af690561e59cd4d26c
garciale24 <c.garciale24@gmail.com>
2022-09-23 12:01:24 -0700

    css and backend stuff

--- src/app/previous-orders/previous-orders.component.ts
+++ src/app/previous-orders/previous-orders.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PreviousOrdersService } from '../previous-orders.service';
+import { Component, OnInit, ɵgetUnknownElementStrictMode } from '@angular/core';
+import { PreviousOrdersService } from './previous-orders.service';
+
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-previous-orders',
@@ -8,7 +11,7 @@ import { PreviousOrdersService } from '../previous-orders.service';
 })
 export class PreviousOrdersComponent implements OnInit {
   previousOrders: any = [];
-  constructor(private previousOrdersService: PreviousOrdersService) {}
+  constructor(private previousOrdersService: PreviousOrdersService,private authService: SocialAuthService) {}  invalidDate: Date = new Date(0);
 
   getPreviousOrders(): void {
     this.previousOrdersService
@@ -19,14 +22,45 @@ export class PreviousOrdersComponent implements OnInit {
       });
     this.previousOrders.sort((a: any, b: any) => {
       return (
-        new Date(a.DateDelivered).getTime() -
-        new Date(b.DateDelivered).getTime()
+        new Date(b.DateOrdered).getTime() - 
+        new Date(a.DateOrdered).getTime()
+      );
+    });
+  }
+  user!: SocialUser;
+  ngOnInit(): void {
+    this.getPreviousOrders();
+
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+    });
+  }
+
+
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  getPreviousOrdersUser(userID: number) {
+    this.previousOrdersService
+      .getPrevOrders(userID)
+      .subscribe((previousOrders) => {
+        this.previousOrders = previousOrders;
+        console.log(this.previousOrders);
+      });
+    this.previousOrders.sort((a: any, b: any) => {
+      return (
+        new Date(b.DateOrdered).getTime() - new Date(a.DateOrdered).getTime()
       );
     });
   }
 
-  ngOnInit(): void {
-    this.getPreviousOrders();
+  cancelPreviousOrder(orderID: number, userID: number) {
+    this.previousOrdersService.cancelOrder(orderID).subscribe((data) => {
+      console.log(data);
+      this.getPreviousOrdersUser(userID);
+    });
   }
-
 }
