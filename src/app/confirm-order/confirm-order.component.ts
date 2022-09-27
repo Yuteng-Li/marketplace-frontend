6500e187cafb74b9c5f5cde539c166b657a33db5
Andrew <23265028+AndrewMurillo@users.noreply.github.com>
2022-09-27 10:07:40 -0700

    A few changes

--- src/app/confirm-order/confirm-order.component.ts
+++ src/app/confirm-order/confirm-order.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart/cart.component.service';
 import { OrderService } from './order.service';
-import { NavigationExtras, Router } from '@angular/router';
-import { Order } from './order';
+import { Router } from '@angular/router';
+import { Order } from './order'; 
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-confirm-order',
@@ -11,11 +13,20 @@ import { Order } from './order';
 })
 export class ConfirmOrderComponent implements OnInit {
   cartItems: any = [];
+  total: number = 0;
   order!: Order;
-  constructor(private cartService: CartService, private orderService: OrderService, private router: Router) { }
+  user!: SocialUser;
+  constructor(private cartService: CartService, private orderService: OrderService,
+     private router: Router, private authService: SocialAuthService) { }
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getCart();
+    this.getTotal();
+    
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+    });
   }
 
   placeOrder(): void {
@@ -25,6 +36,12 @@ export class ConfirmOrderComponent implements OnInit {
         this.router.navigate(["/home-page"]);
       } 
     );
+  }
+
+  getTotal(): void{
+    this.cartItems.array.forEach((element: { itemPrice: number; itemQty: number; }) => {
+      this.total += element.itemPrice * element.itemQty;
+    });
   }
 
 }
