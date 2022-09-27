11568c89dcb6c686f49cd9adc91dcc461e2531f3
Arian <aryannamavar@gmail.com>
2022-09-27 14:58:43 -0700

    Attempting to convert requests into forkJoin observables

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import {Observable, throwError} from "rxjs";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
+import {forkJoin, Observable, throwError} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
+
 
 
 @Component({
@@ -20,12 +29,12 @@ export class AddressFormComponent implements OnInit{
   Below we include the different form fields that are needed for display, and HTTP method purposes (Post, put,...)
    */
 
-  // userData is in this format because I need to add user info into this userData and send this object to HTTP Methods
+  // user_data is in this format because I need to add user info into this user_data and send this object to HTTP Methods
   // without the {[index: string]:any} i can't really index or add anything to the object
-  userData: {[index: string]:any} = {};
-  userID?:string;
-  addressid?:string;
-  recipient_name?:string;
+  user_data: {[index: string]:any} = {};
+  user_id?:string;
+  address_id?:string;
+  recipient_name:string='';
   street?:string;
   street2?:string;
   city?:string;
@@ -33,20 +42,29 @@ export class AddressFormComponent implements OnInit{
   zip?:string;
 
 
+  // validation patterns for address form
+  state_pattern:string = '^[a-zA-Z][a-zA-Z]$';
+  recipient_pattern:string = '^[A-Za-z ,.\'\\-]*$';
+  city_pattern:string = '^[A-Za-z ,.\'\\-]{1,30}$';
+  zip_pattern:string = '^[0-9]{5}$';
+
+
+
+
   // the form builder making the different fields with a generic validator for now
   addressForm = this.fb.group
   (
     {
-      // addressid and userid are included in the form code, BUT!! not presented to the user, as they don't need to
+      // address_id and user_id are included in the form code, BUT!! not presented to the user, as they don't need to
       // know their ids, these 2 fields are just kept for ease of coding(sending the PUT/Post/Patch... requests)
-      addressid: [''],
-      userid: [''],
-      recipient_name: ['', Validators.required],
-      street: ['', Validators.required],
-      street2: [''],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      zip: ['', Validators.required],
+      address_id: [''],
+      user_id: [''],
+      recipient_name: ['', [Validators.required, Validators.pattern(this.recipient_pattern), Validators.maxLength(50)] ],
+      street: ['', [Validators.required, Validators.maxLength(30) ] ],
+      street2: ['', Validators.maxLength(30)],
+      city: ['', [Validators.required, Validators.pattern(this.city_pattern)] ],
+      state: ['', [  Validators.required, Validators.pattern(this.state_pattern)  ]],
+      zip: ['', [Validators.required, Validators.pattern(this.zip_pattern)]  ],
       is_shipping: ['true'],
       is_billing: ['true'],
     }
@@ -66,39 +84,89 @@ export class AddressFormComponent implements OnInit{
     // after user logs in, GET their info from the DB, and assign the result to various vars specified for storage
     // and HTTP methods
     console.log(`The SSO provided user is: ${this.user.email}\n\n\n`);
-    this.http.get(`http://localhost:${this.port_number}/api/address/getAddress/${1}`)
+
+
+
+
+    // this.findAddressByName(this.user.name);
+    console.log("after findaddressbyname FUNCTION");
+    // this.recipient_name = this.user.name;
+    console.log(`name found in ngOnInit is ${this.recipient_name}`);
+    console.log(`address found in ngOnInit is ${this.address_id}`);
+    // this.setAddressFields();
+  }
+
+  private setAddressFields()
+  {
+    this.http.get(`http://localhost:${this.port_number}/api/address/getAddress/${this.address_id}`)
       .subscribe
       (data =>
         {
           console.log(data);
-          this.userData = data;
-          this.userID = this.userData['userID'];
-          this.addressid = this.userData['addressID'];
-          this.recipient_name = this.userData['recipientName'];
-          this.street = this.userData['street'];
-          this.street2 = this.userData['street2'];
-          this.state = this.userData['state'];
-          this.zip = this.userData['zip'];
-          this.city = this.userData['city'];
+          this.user_data = data;
+          this.user_id = this.user_data['user_id'];
+          this.address_id = this.user_data['address_id'];
+          this.recipient_name = this.user_data['recipient_name'];
+          this.street = this.user_data['street'];
+          this.street2 = this.user_data['street2'];
+          this.state = this.user_data['state'];
+          this.zip = this.user_data['zip'];
+          this.city = this.user_data['city'];
         }
       );
-
-
-
   }
 
 
-  onSubmit(): void
+  private findAddressByName(name: string) {
+    let searchedAddressId:string = "0";
+    let list_all_users: {  [index: string]:any  } = {};
+    this.http.get(`http://localhost:${this.port_number}/api/address/getAllAddresses`)
+      .subscribe
+      (
+        allUsers =>
+        {
+          list_all_users = allUsers;
+          let user_index:number = 0;
+          while (true)
+          {
+            if (list_all_users[user_index] == undefined)
+            {
+              break;
+            }
+
+            if (list_all_users[user_index]['recipient_name'] === this.user.name)
+            {
+              console.log("MATCH FOUND IN findaddressByName");
+              console.log(list_all_users[user_index]['address_id']);
+              console.log(list_all_users[user_index]['user_id']);
+              this.address_id = list_all_users[user_index]['address_id'];
+              this.user_id =list_all_users[user_index]['user_id'];
+              searchedAddressId = list_all_users[user_index]['address_id'];
+            }
+            // console.log(list_all_users[user_index]['recipient_name']);
+            // console.log(this.user.name);
+            // console.log(list_all_users[user_index]['address_id']);
+            user_index+=1;
+
+          }
+        }
+      )
+
+    return searchedAddressId;
+  }
+
+
+  onSubmitUpdate(): void
   {
 
-    if (this.addressForm.valid == true)
+    if (this.addressForm.valid)
     {
-      // once all the required fields of the FORMBUILDER is valid, then add the userID and addressID to the FB,
+      // once all the required fields of the FORMBUILDER is valid, then add the user_id and addressID to the FB,
       // and send for PUT request.
-      this.addressForm.value.userid = this.userID;
-      this.addressForm.value.addressid = this.addressid;
-
-      this.http.put(`http://localhost:9090/api/updateAddress/${this.addressid}`, this.addressForm.value)
+      this.addressForm.value.user_id = this.user_id;
+      this.addressForm.value.address_id = this.address_id;
+      console.log(this.addressForm.value);
+      this.http.put(`http://localhost:9090/api/address/updateAddress/${this.address_id}`, this.addressForm.value)
         .subscribe
           (resp=>
             {
@@ -109,16 +177,41 @@ export class AddressFormComponent implements OnInit{
     // console.log('You entered value: ', this.addressForm.value);
   }
 
+
+  onSubmitAdd(): void
+  {
+    // NOTE! if no address on file. THEN you can't add address (cold-start)
+    // since user-id is needed.
+    if (this.addressForm.valid)
+    {
+      // once all the required fields of the FORMBUILDER is valid, then add the user_id and addressID to the FB,
+      // and send for PUT request.
+      this.addressForm.value.user_id = this.user_id;
+      delete this.addressForm.value.address_id;
+      let new_user: {  [index: string]:any  } = {};
+      console.log(this.addressForm.value);
+      this.http.post(`http://localhost:9090/api/address/createAddress`, this.addressForm.value)
+        .subscribe
+        (resp=>
+          {
+            new_user = resp;
+            this.address_id = new_user['address_id'];
+            console.log(resp);
+          }
+        );
+    }
+  }
+
   deleteAddress():void
   {
     console.log("Delete button clicked");
-    console.log(`http://localhost:${this.port_number}/api/address/deleteAddress/1`)
+    console.log(`http://localhost:${this.port_number}/api/address/deleteAddress/${this.address_id}`)
     this.http.delete
     (
-      `http://localhost:${this.port_number}/api/address/deleteAddress/1`
+      `http://localhost:${this.port_number}/api/address/deleteAddress/${this.address_id}`
     ).subscribe(() => console.log("Deleted"));
-    this.userID =  '';
-    this.addressid =  '';
+    this.user_id =  '';
+    this.address_id =  '';
     this.recipient_name =  '';
     this.street = '';
     this.street2 =  '';
@@ -127,10 +220,11 @@ export class AddressFormComponent implements OnInit{
     this.city = '';
   }
 
-  userLog(): void
+  addUser():void
   {
-    // STUB
+    console.log("Add STUB");
   }
+
 }
 
 
