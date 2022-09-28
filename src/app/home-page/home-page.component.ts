8cb8ecd835775ebbad8e4236585e2abab6518d71
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2022-09-28 11:53:36 -0700

    Got category shortcuts on homepage to work. Routes to category page, and uses mock categories as of now.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoryService } from '../categories/category.service';
+import { Category } from '../categories/Category';
+import { CATEGORIES } from '../categories/mock-categories';
+
 import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { SocialUser } from '@abacritt/angularx-social-login';
 
@@ -11,18 +15,25 @@ import { SocialUser } from '@abacritt/angularx-social-login';
 export class HomePageComponent implements OnInit {
 
   user!: SocialUser;
+  catCard : Category[] = [];
 
-  constructor(private authService: SocialAuthService) { }
+  constructor(private authService: SocialAuthService, private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
       console.log(user);
     });
+
+    this.getCategories();
   }
 
   signOut(): void {
     this.authService.signOut();
+  }
+
+  getCategories(): void {
+    this.categoryService.getCategories().subscribe( (catCard: Category[]) => {this.catCard = catCard; console.log(this.catCard)});
   }
 
 }
