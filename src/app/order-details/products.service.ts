59655a9e207ad07b1e3cddeae98847b9cbd5a8d2
Teezey <yibiaojob@gmail.com>
2022-09-30 16:47:51 -0700

    changing the port number

--- src/app/order-details/products.service.ts
+++ src/app/order-details/products.service.ts
@@ -9,11 +9,7 @@ import { Product } from '../shared/Product';
 export class ProductsService {
 
   //url from inventory team api
-<<<<<<< HEAD
-  private productUrl = "http://localhost:8080/api/products"; // Inventory Backend
-=======
   private productUrl = "http://localhost:8081/api/products";
->>>>>>> 5ca718b427c0e7aed69e362da0afb9007d836b41
   constructor(private http:HttpClient){}
 
   getProductsId(id: String) : Observable<any>{ 
